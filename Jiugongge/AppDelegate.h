ee1efb259765ec0af86ae22cd53e5d3fc2c3d179
abel <abel@abeldemacbook-pro.local>
2015-08-10 23:50:48 +0800

    Initial Commit

---
+++ Jiugongge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Jiugongge
+//
+//  Created by abel on 15/8/10.
+//  Copyright © 2015年 abel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Jiugongge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Jiugongge
+//
+//  Created by abel on 15/8/10.
+//  Copyright © 2015年 abel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
